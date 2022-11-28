19fa26fb893a90af255a926336e8c5b832a0d7f7
na1stone <fheoa700000@naver.com>
2022-11-29 01:57:41 +0900

    css 수정

--- src/Woobin.tsx
+++ src/Woobin.tsx
@@ -1,33 +1,34 @@
 import React, {useState} from "react";
 
-const Woobin=()=>{ 
-    const [nameList, setNameList] = useState(['손지원','나원석','다람쥐','라면'])
-    const arrayLength: number = nameList.length
-    const filtername:string[] = nameList.filter((value) => value !== "김우빈")
-    const splicename = nameList.splice
-    const slicename = nameList.slice(-1)
+const Woobin = () => {
+  const [nameList, setNameList] = useState(['손지원', '나원석', '다람쥐', '라면'])
+  const arrayLength: number = nameList.length
+  const filtername: string[] = nameList.filter((value) => value !== "김우빈")
+  const splicename = nameList.splice
+  const slicename = nameList.slice(-1)
 
 
-    return(
-        <div>
-            {nameList}
-            <button onClick={() => setNameList(nameList.concat('김우빈'))}>
-                김우빈 추가
-            </button>
+  return (
+    <div style={{display: "flex", flexDirection: "column", gap:"25px"}}>
+      <div style={{display: "flex", gap: "10px"}}>
+        {nameList}
+      </div>
+      <div>
+        <button onClick={() => setNameList(nameList.concat('김우빈'))}>
+          김우빈 추가
+        </button>
+        <button onClick={() => filtername}>
+          김우빈 삭제
+        </button>
+        <button onClick={() => filtername}>
+        </button>
+      </div>
 
-            <button onClick={() => filtername}>
-                김우빈 삭제
-            </button>
 
-            <button onClick={() => filtername}>
-                
-            </button>
+    </div>
 
+  )
 
-        </div>
+}
 
-    )
-
- }
-
- export default Woobin;
+export default Woobin;
