a54b5fffaa407743b03c5d1e71f3f6b08fe69408
suzu999 <wonaji0804@gmail.com>
2022-11-29 01:55:57 +0900

    우빈씨 추가 성공

--- src/Woobin.tsx
+++ src/Woobin.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 
 const Woobin=()=>{ 
-    const [nameList, setNameList] = useState(['가','나','다','라'])
+    const [nameList, setNameList] = useState(['손지원','나원석','다람쥐','라면'])
     const arrayLength: number = nameList.length
     const filtername:string[] = nameList.filter((value) => value !== "김우빈")
     const splicename = nameList.splice
@@ -10,10 +10,17 @@ const Woobin=()=>{
 
     return(
         <div>
+            {nameList}
             <button onClick={() => setNameList(nameList.concat('김우빈'))}>
+                김우빈 추가
             </button>
 
             <button onClick={() => filtername}>
+                김우빈 삭제
+            </button>
+
+            <button onClick={() => filtername}>
+                
             </button>
 
 
