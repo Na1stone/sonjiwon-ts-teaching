5fed7114470b269f6eac821396e3fead496379b6
suzu999 <wonaji0804@gmail.com>
2022-11-29 02:20:28 +0900

    김우빈삭제,두번째놈출력

--- src/Woobin.tsx
+++ src/Woobin.tsx
@@ -4,8 +4,7 @@ const Woobin = () => {
   const [nameList, setNameList] = useState(['손지원', '나원석', '다람쥐', '라면'])
   const arrayLength: number = nameList.length
   const filtername: string[] = nameList.filter((value) => value !== "김우빈")
-  const splicename = nameList.splice
-  const slicename = nameList.slice(-1)
+  const slicename:string[] = nameList.slice(0,3)
 
 
   return (
@@ -17,11 +16,21 @@ const Woobin = () => {
         <button onClick={() => setNameList(nameList.concat('김우빈'))}>
           김우빈 추가
         </button>
-        <button onClick={() => filtername}>
+
+        <button onClick={() => setNameList(filtername)}>
           김우빈 삭제
         </button>
-        <button onClick={() => filtername}>
+
+        <button onClick={() => setNameList(nameList.splice(1,1))}>
+          두번째 있는 놈 출력
         </button>
+
+        <button onClick={() => setNameList(slicename)}>
+          라면 삭제
+        </button>
+
+
+
       </div>
 
 
